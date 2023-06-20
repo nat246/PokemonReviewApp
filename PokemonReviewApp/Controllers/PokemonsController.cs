3aea3953031b85b399f12da64fcfdb85b8ea415c
Nathanael Ang <natnatang@hotmail.com>
2023-06-21 08:17:18 +1000

    Add project files.

---
+++ PokemonReviewApp/Controllers/PokemonsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonsController : Controller
+    {
+        private readonly IPokemonRepository pokemonRepository;
+
+        public PokemonsController(IPokemonRepository pokemonRepository)
+        {
+            this.pokemonRepository = pokemonRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
+        public IActionResult GetPokemons()
+        {
+            var pokemons = pokemonRepository.GetPokemons();
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok(pokemons);
+        }
+    }
+}
---
+++ PokemonReviewApp/Repository/PokemonRepository.cs
@@ -0,0 +1,23 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository
+{
+    public class PokemonRepository : IPokemonRepository
+    {
+        private readonly DataContext context;
+
+        public PokemonRepository(DataContext context)
+        {
+            this.context = context;
+        }
+
+        public ICollection<Pokemon> GetPokemons()
+        {
+            return context.Pokemons.OrderBy(p => p.Id).ToList();
+        }
+
+
+    }
+}
